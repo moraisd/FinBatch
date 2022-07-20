5abe80c9276e7b26c5b373e4f237c0de9d07cd29
Daniel <moraisd@protonmail.com>
2022-07-20 20:05:32 -0300

    Refactorings

--- tests/test_eod_reader.py
+++ tests/test_eod_reader.py
@@ -1,15 +1,14 @@
 import os.path
 from unittest import TestCase
 
-import symbol.file_reader as eod_reader
-
 from config.config_reader import get_root_dir
+from symbol import eod_reader
 
 
-class TestEodCsvFileReader(TestCase):
+class TestEodReader(TestCase):
 
     def test_retrieve_non_otc_stocks_only(self):
-        with open(os.path.join(get_root_dir(), os.path.dirname(__file__), 'sample_stock_csv_data.csv'), 'r',
+        with open(os.path.join(get_root_dir(), os.path.dirname(__file__), 'sample_eod_symbol_data.csv'), 'r',
                   newline='\r') as file:  # do not split newlines '\n' to simulate REST data
-            data = eod_reader.read_csv(file.readlines()[0])
+            data = eod_reader.read(file.readlines()[0])
             self.assertSetEqual(data, {'AA', 'AACIW', 'AADI', 'AACI', 'AACIU', 'A', 'AAC', 'AA-CG'})
