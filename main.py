e6448e61805129887cc8c8b423a4de83b3872e98
Daniel <moraisd@protonmail.com>
2025-03-26 14:56:03 -0300

    FIN-7: Implement Non-Blocking APScheduler

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from scheduler.jobs_manager import (schedule_symbol_job, run_jobs, add_listeners,
-                                    schedule_update_stocks_job)
+import asyncio
 
-if __name__ == '__main__':
+from scheduler.jobs_manager import (schedule_symbol_job, run_jobs, add_listeners,
+                                    schedule_update_stocks_job, scheduler_shutdown)
+
+
+async def main():
     schedule_symbol_job()
     schedule_update_stocks_job()
     add_listeners()
     run_jobs()
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_forever()
+    except (KeyboardInterrupt, Exception):
+        # Gracefully shut down on Ctrl+C
+        print("\nShutting down scheduler...")
+        scheduler_shutdown()
+        loop.stop()
+        print("Scheduler and event loop stopped.")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
