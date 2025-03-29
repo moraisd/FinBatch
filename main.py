6cdf78e8aa73c709b7b1ec50867e338e33a2e670
Daniel <moraisd@protonmail.com>
2025-03-29 17:22:37 -0300

    FIN-6: Cleanup

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import asyncio
 import logging
-from asyncio import CancelledError
 
 from scheduler.jobs_manager import (schedule_symbol_job, run_jobs, add_listeners,
                                     schedule_update_stocks_job)
 
 _log = logging.getLogger(__name__)
+
 
 async def main():
     schedule_symbol_job()
@@ -18,5 +18,5 @@ async def main():
 if __name__ == '__main__':
     try:
         asyncio.run(main())
-    except (KeyboardInterrupt, CancelledError):
+    except KeyboardInterrupt:
         _log.info('Application stopped')
